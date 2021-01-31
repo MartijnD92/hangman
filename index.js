ee744c224889abe7c7d58b68b218af0e7ed702df
Martijn Doensen <martijn.doensen@live.nl>
2021-01-31 15:51:23 +0100

    Finish game design, ready for approval

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
+
 const { question } = require("readline-sync");
-const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
+const { displayWordSoFar, isGameWon, turnsTillLost, drawGallows, isGameLost } = require("./gamelogic");
 
 function game(word, guesses) {
-  console.log("Dit heb je tot nu toe geraden: ", guesses);
+    word = word.toUpperCase();
+    const wordSoFar = displayWordSoFar(word, guesses);
+    let turnsLeft = turnsTillLost(word, guesses);
 
-  const letter = question("Raad een letter: ");
+    console.log(`\nDit heb je tot nu toe geraden:\n ${wordSoFar}\n`);
 
-  // voeg de geraden letter toe aan de array met guesses
-  guesses.push(letter);
+    drawGallows(turnsLeft);
 
-  // volgende ronde! we roepen game nog een keer aan
-  game(word, guesses);
+    const letter = question(`Raad een letter (nog ${turnsLeft} ${turnsLeft === 1 ? "kans" : "kansen"}): `).toUpperCase();
+
+    if (letter.length > 1) {
+        console.log("\nJe mag slechts één letter per keer raden. Probeer het opnieuw.");
+        turnsLeft--;
+    }
+
+    // voeg de geraden letter toe aan de array met guesses
+    guesses.push(letter);
+
+    // We controleren of we hebben gewonnen
+    if (isGameWon(word, guesses)) {
+        console.log(`
+        Hoera! Je hebt het woord geraden!
+        **** ${word} ****
+        `);
+        return;
+    }
+
+    // We controleren of we hebben verloren
+    if (isGameLost(word, guesses)) {
+        console.log(`
+        Jammer! Je hebt het woord niet snel genoeg geraden!
+        **** ${word} ****
+
+        ~D~E~A~D~
+        -----------
+        | /      |
+        |/      _o_
+        |        0
+        |       / \\
+        |           
+        ===========
+        `);
+        return;
+    }
+
+    // volgende ronde! we roepen game nog een keer aan
+    game(word,guesses);
 }
 
 console.log(`
@@ -23,4 +62,6 @@ __________
 ===========  ░╚═════╝░╚═╝░░╚═╝╚══════╝░╚═════╝░░╚════╝░╚══════╝
 `);
 
-game("javascript", []);
+
+game("pandabeertje", []);
+
